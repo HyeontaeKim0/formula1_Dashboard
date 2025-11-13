bd399975cb8c40cb252df6ad67185a1ed0688e67
HyeontaeKim0 <hyeontae.kim@wincl.io>
2025-11-13 11:17:38 +0900

    feat : profile image update

--- lib/openf1.ts
+++ lib/openf1.ts
@@ -1,3 +1,47 @@
+// McLaren
+import Piastri from "../assets/img/driverProfile/McLaren/piastri.webp";
+import Norris from "../assets/img/driverProfile/McLaren/norris.webp";
+import McLarenLogo from "../assets/img/teamLogo/McLaren.webp";
+
+// Aston Martin
+import Stroll from "../assets/img/driverProfile/AstonMartin/stroll.webp";
+import Alonso from "../assets/img/driverProfile/AstonMartin/fernando.webp";
+import AstonMartinLogo from "../assets/img/teamLogo/Martin.svg";
+
+// Red Bull
+import Verstappen from "../assets/img/driverProfile/RedBull/max.webp";
+import Tsunoda from "../assets/img/driverProfile/RedBull/yuki.webp";
+
+import RedBullLogo from "../assets/img/teamLogo/RedBull.svg";
+// Ferrari
+import Hamilton from "../assets/img/driverProfile/Ferrari/hamilton.webp";
+import Leclerc from "../assets/img/driverProfile/Ferrari/leclerc.webp";
+import FerrariLogo from "../assets/img/teamLogo/Ferrari.svg";
+// Mercedes
+import Russell from "../assets/img/driverProfile/Mercedes/george.webp";
+import Kimi from "../assets/img/driverProfile/Mercedes/kimi.webp";
+import MercedesLogo from "../assets/img/teamLogo/Mercedes.svg";
+// Williams
+import Sainz from "../assets/img/driverProfile/Williams/sainz.webp";
+import Albon from "../assets/img/driverProfile/Williams/albon.webp";
+import WilliamsLogo from "../assets/img/teamLogo/Williams.svg";
+// Haas
+import Ocon from "../assets/img/driverProfile/Haas/ocon.webp";
+import Oliver from "../assets/img/driverProfile/Haas/oliver.webp";
+import HaasLogo from "../assets/img/teamLogo/Haas.svg";
+// Alpine
+import Gasly from "../assets/img/driverProfile/Alpine/gasly.webp";
+import Franco from "../assets/img/driverProfile/Alpine/franco.webp";
+import AlpineLogo from "../assets/img/teamLogo/Alpine.svg";
+// Kick Sauber
+import Niko from "../assets/img/driverProfile/KickSauber/niko.webp";
+import Gabriel from "../assets/img/driverProfile/KickSauber/gabriel.webp";
+import KickSauberLogo from "../assets/img/teamLogo/Kick.svg";
+// Racing bulls
+import Isack from "../assets/img/driverProfile/Racingbulls/isack.webp";
+import Liam from "../assets/img/driverProfile/Racingbulls/lowson.webp";
+import RacingBullsLogo from "../assets/img/teamLogo/RacingBulls.svg";
+
 export interface OpenF1Driver {
   broadcast_name: string;
   country_code: string;
@@ -31,25 +75,16 @@ export interface Driver {
 
 // 팀 로고 URL 매핑
 const teamLogoMap: Record<string, string> = {
-  "Red Bull Racing":
-    "https://i.namu.wiki/i/tmc7BkeuZHgon5-SUmoPnWjIJMHXK-9iKxwoaPVwgkfjoTpCfBDocni5QPxicnfuLvUu0w6F8x5OOYskdAhzYA.svg",
-  McLaren:
-    "https://i.namu.wiki/i/NMVkodqAXkSDUBnIu-PG7rHKsJSdUj-M7zfEW3i1aoB_QWP2QC5CaJT90iK0wJpzefENKLG-Fhtd59o1Qr1qHQ.webp",
-  Ferrari:
-    "https://i.namu.wiki/i/ECrvNlNmJzF6cqUToZ0rAzY7or8LZUSccqLj2H6KyEW4aJj1ZgQZLY4bhgA5A87WcQHIqY6_RqX23r6tZQCa8g.svg",
-  Mercedes:
-    "https://i.namu.wiki/i/1xsredTlZXOc_NkIFckcHDXRvlsPGZ0PmUYib8hZrdf_v1yT0aUjulwXeldN7m7CLP8mO_NwyW6UvjF-hEl73Jw1FgTtq6cT2EVmCnxWz2ConAvSzbWomzQQykVJn3O-aJ4onwHDm0Yud7M-9xuX6A.svg",
-  "Aston Martin":
-    "https://i.namu.wiki/i/RcgIM1PvyryM8UU0J-Ytt3IVS6z3FEghJYBwTQ2AGnUh89V0fN1uenv_OCaL0_JfhX4byMZUvyFLEOerSUoHtad4PBhg4piYaYxcaTgpjLFd-fffIpa0o-QRZvhe3a-Kz3CQ3nZfzUKIgLESs3FEhg.svg",
-  "Kick Sauber":
-    "https://i.namu.wiki/i/136BQjIm9zqHcpmJyJB67rk4NWDofehAID_tR-mEs7FxT6vyAhClZMHJJCzP6uZiNNxnJj0KuFpS9yXTbHFE9A.svg",
-  Alpine:
-    "https://i.namu.wiki/i/4vhnPWoRW7K8QtIRG1X_hG6E6clBWqOJnDFf-mMbKl3WpJSfud1Un7aIRWjEYuZH9tERb_CNW9u4qsIZVvZX_1YVRhYiDbxYVRQBbNs7ucQK0Fz2AzgOyzHILLDAMNyAh6IgJ-pb6z2G4u1WgJLryA.svg",
-  Williams:
-    "https://i.namu.wiki/i/0vJLUUPLuLAQiOXZhkLC6CVckyCPJjB4LT0V4boG9RzqyEJVkbj-7E1mZOV7T1uVdoJCyTVw-TMx5SABTvSE1A.svg",
-  Haas: "https://i.namu.wiki/i/BbahTUJdRaBbw82_y3Sg1WpuNv2SJmE9QKhR2N8vLvJEdcJvhSyStL_zxgmwRJkIwAZCE6o787jxdZR_rHRvn01SR5HXq_3z1kWmqxpJsb2VcC0mUPn9fiG_iGkjqiId6Crul4JiQBgRHhZrAiSCSQ.svg",
-
-  RB: "https://i.namu.wiki/i/GS9rp5MI79PwBFESgp04FG4Gu7OXAH3VduJEnCE0_w7xI_aU-TB9yrMC8-5LymjuOP-8U6Rwoikn6IWVpAnW2A.svg",
+  "Red Bull Racing": RedBullLogo.src || "",
+  McLaren: McLarenLogo.src || "",
+  Ferrari: FerrariLogo.src || "",
+  Mercedes: MercedesLogo.src || "",
+  "Aston Martin": AstonMartinLogo.src || "",
+  "Kick Sauber": KickSauberLogo.src || "",
+  Alpine: AlpineLogo.src || "",
+  Williams: WilliamsLogo.src || "",
+  "Haas F1 Team": HaasLogo.src || "",
+  "Racing Bulls": RacingBullsLogo.src || "",
 };
 
 // 팀 이름 한글 매핑
@@ -63,7 +98,8 @@ const teamNameMap: Record<string, string> = {
   "Haas F1 Team": "하스",
   Williams: "윌리엄스",
   RB: "RB",
-  Sauber: "킥 자우버",
+  "Kick Sauber": "킥 자우버",
+  "Racing Bulls": "레이싱 불스",
 };
 
 // 국가 코드 한글 매핑
@@ -84,92 +120,48 @@ const countryCodeMap: Record<string, string> = {
 
 // 드라이버 얼굴 사진 맵
 const driverHeadshotMap: Record<string, string> = {
-  "Max VERSTAPPEN":
-    "https://i.namu.wiki/i/nE2jIeuRXhG_Q43a1IQCy7T7frTKvGrHjWnUH2izuzXRDzB0t5xX4NAuvO30i6QQ8Di8M3gdjVNgalpLEcosVP56clMjcZ8EWQxp-OH3znxFtea-3RvJ7hefFYL_hij8pU79466RFm_pY93qCElosw.webp",
-  "Max Verstappen":
-    "https://i.namu.wiki/i/nE2jIeuRXhG_Q43a1IQCy7T7frTKvGrHjWnUH2izuzXRDzB0t5xX4NAuvO30i6QQ8Di8M3gdjVNgalpLEcosVP56clMjcZ8EWQxp-OH3znxFtea-3RvJ7hefFYL_hij8pU79466RFm_pY93qCElosw.webp",
-  "Charles LECLERC":
-    "https://i.namu.wiki/i/VcibXppQlW66pEXfJlcxVtIy3FPHlecmSv_ndjaDs0g7tW7sWW2QS2HNwNPGT1--z1sXWRkEACJggOv9352SzaV4iRl0ajgK98cPAsQVnZ6XauOe_wpgLYfmThcQnmJxRtlXEnlghxYZ-5HsSbROEg.webp",
-  "Charles Leclerc":
-    "https://i.namu.wiki/i/VcibXppQlW66pEXfJlcxVtIy3FPHlecmSv_ndjaDs0g7tW7sWW2QS2HNwNPGT1--z1sXWRkEACJggOv9352SzaV4iRl0ajgK98cPAsQVnZ6XauOe_wpgLYfmThcQnmJxRtlXEnlghxYZ-5HsSbROEg.webp",
-  "Lewis HAMILTON":
-    "https://i.namu.wiki/i/DPpz7hWHe8QXAHrD-MUXB-ha0cxUN4roXTtGq5zFbgSk6ZbBYZJC6IrJGxBsoqb5FJi3bDnrpRzRIRyKb0pskVTlKe3EeNAxTL6tQ7l47gsVS7eSs6v1KXVP8WR7oTOFr4vCutnbdgfpFU-ukGSX5g.webp",
-  "Lewis Hamilton":
-    "https://i.namu.wiki/i/DPpz7hWHe8QXAHrD-MUXB-ha0cxUN4roXTtGq5zFbgSk6ZbBYZJC6IrJGxBsoqb5FJi3bDnrpRzRIRyKb0pskVTlKe3EeNAxTL6tQ7l47gsVS7eSs6v1KXVP8WR7oTOFr4vCutnbdgfpFU-ukGSX5g.webp",
-  "George RUSSELL":
-    "https://i.namu.wiki/i/wi5vieXPKvZiN9WsJE0t4N0geDiKIYdXYHb9ME6PAvalFyuG8ZYH7xBrjTxICaJsrjHEHhVS1WRag38Zb6BG7MQy8V9I_MCMcrBNHedIGUeIFcc7BgT2JLtdVyvMvnke1kUzXF0WnzgghvFKiI6C-g.webp",
-  "George Russell":
-    "https://i.namu.wiki/i/wi5vieXPKvZiN9WsJE0t4N0geDiKIYdXYHb9ME6PAvalFyuG8ZYH7xBrjTxICaJsrjHEHhVS1WRag38Zb6BG7MQy8V9I_MCMcrBNHedIGUeIFcc7BgT2JLtdVyvMvnke1kUzXF0WnzgghvFKiI6C-g.webp",
-  "Carlos SAINZ":
-    "https://i.namu.wiki/i/ccjbqgz-gyk-BIFr3EKN0WUkz_XgSKZRG_6gxxMx393E51Eoh8mwS2TK5PhAwqNrxdejGzCZeqokylVkrIZ81rvFvV10xUGcUwUubLuMJ2DGJZvQqBQVnBPlhitxjZbXybg1mJe7slcGlSmmOGeFHw.webp",
-  "Carlos Sainz":
-    "https://i.namu.wiki/i/ccjbqgz-gyk-BIFr3EKN0WUkz_XgSKZRG_6gxxMx393E51Eoh8mwS2TK5PhAwqNrxdejGzCZeqokylVkrIZ81rvFvV10xUGcUwUubLuMJ2DGJZvQqBQVnBPlhitxjZbXybg1mJe7slcGlSmmOGeFHw.webp",
-  "Carlos Sainz Jr.":
-    "https://i.namu.wiki/i/ccjbqgz-gyk-BIFr3EKN0WUkz_XgSKZRG_6gxxMx393E51Eoh8mwS2TK5PhAwqNrxdejGzCZeqokylVkrIZ81rvFvV10xUGcUwUubLuMJ2DGJZvQqBQVnBPlhitxjZbXybg1mJe7slcGlSmmOGeFHw.webp",
-  "Lando NORRIS":
-    "https://i.namu.wiki/i/KlqVu1GUrEXhFUJG04WfEIKuN-UElLmFN2e3VFRLA6NcH4QP_vZ6A46ek8OX0fm2HbiFOrOPtC31N-RiVbdLgN87Wmy17n5cHPbDYVdqicWP6wK4pA_QrSZZULITAEe8k_ZTaon5e2diemhh9ORLzw.webp",
-  "Lando Norris":
-    "https://i.namu.wiki/i/KlqVu1GUrEXhFUJG04WfEIKuN-UElLmFN2e3VFRLA6NcH4QP_vZ6A46ek8OX0fm2HbiFOrOPtC31N-RiVbdLgN87Wmy17n5cHPbDYVdqicWP6wK4pA_QrSZZULITAEe8k_ZTaon5e2diemhh9ORLzw.webp",
-  "Sergio PEREZ":
-    "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_auto/content/dam/fom-website/2018-redesign-assets/drivers/2025/sergeipe01.png",
-  "Sergio Perez":
-    "https://media.formula1.com/image/upload/f_auto,c_limit,q_75,w_auto/content/dam/fom-website/2018-redesign-assets/drivers/2025/sergeipe01.png",
-  "Fernando ALONSO":
-    "https://i.namu.wiki/i/mGgW_1IbrvGe_iaxtyDW7BPdFy_YAhqGVy1ErgHG-_SRu8rZcKE3_7XVRQD0ugmEjp5-foHPp4XLvZI2KzSFJxJLOAoHa90IBYB-qWUL-v8RYBGWUDCF3kCx0Lvpa22AKj3hLKeJVSmZyHpwkg_IjQ.webp",
-  "Fernando Alonso":
-    "https://i.namu.wiki/i/mGgW_1IbrvGe_iaxtyDW7BPdFy_YAhqGVy1ErgHG-_SRu8rZcKE3_7XVRQD0ugmEjp5-foHPp4XLvZI2KzSFJxJLOAoHa90IBYB-qWUL-v8RYBGWUDCF3kCx0Lvpa22AKj3hLKeJVSmZyHpwkg_IjQ.webp",
-  "Esteban OCON":
-    "https://i.namu.wiki/i/c5V5nCiURMs5dZgotUWFsM4z02nZQo1sUaYvxed9GP5R5xzy--2Croh2W8nMm_Yv_t7NcE6mgiXqFNPDSTFmzlcGtAuEv02Y4utbVP0K8pjCAOSif_2w06GOi39E84xAWZbNinczgsPea0IFKa_8Jg.webp",
-  "Esteban Ocon":
-    "https://i.namu.wiki/i/c5V5nCiURMs5dZgotUWFsM4z02nZQo1sUaYvxed9GP5R5xzy--2Croh2W8nMm_Yv_t7NcE6mgiXqFNPDSTFmzlcGtAuEv02Y4utbVP0K8pjCAOSif_2w06GOi39E84xAWZbNinczgsPea0IFKa_8Jg.webp",
-  "Pierre GASLY":
-    "https://i.namu.wiki/i/Rit7ZBzEQ8TeDsfIUYcYUZm1jDn4mhvRKpny0OSaxddpeMcZJj7F_FvsB5Btgpu5jV2brbW2CjHUDUPki8QbQiygelN6bdc0GrXem0USq7lAUvWu7Za3p90rkxHjm75Qlr0aQjLa7jVaoml6pfJyQw.webp",
-  "Pierre Gasly":
-    "https://i.namu.wiki/i/Rit7ZBzEQ8TeDsfIUYcYUZm1jDn4mhvRKpny0OSaxddpeMcZJj7F_FvsB5Btgpu5jV2brbW2CjHUDUPki8QbQiygelN6bdc0GrXem0USq7lAUvWu7Za3p90rkxHjm75Qlr0aQjLa7jVaoml6pfJyQw.webp",
-  "Yuki TSUNODA":
-    "https://i.namu.wiki/i/LvX01Es-VZpZ0VYsDHSN2JG28XBeCErYyejGbkBZrJL4S0BjCJmSZHeDOlB3YRAvP_2QZehbxwupuzWfFla5QtL_CjldTzMcW7QHiAVugB2OUh3tPSh8v3T13r0h33P2KpzKDZN1jkmpMd6z6ZA23A.webp",
-  "Yuki Tsunoda":
-    "https://i.namu.wiki/i/LvX01Es-VZpZ0VYsDHSN2JG28XBeCErYyejGbkBZrJL4S0BjCJmSZHeDOlB3YRAvP_2QZehbxwupuzWfFla5QtL_CjldTzMcW7QHiAVugB2OUh3tPSh8v3T13r0h33P2KpzKDZN1jkmpMd6z6ZA23A.webp",
-  "Oscar PIASTRI":
-    "https://i.namu.wiki/i/Ifns07jGT5Z7ACSfDmR6pkdkhB9XvizLt3nBFYDP0NheWksnplmbK7XSHvDmeW4Gzf7YbcsfXyIkhVz5A9xzEq--AUGi6wGLvKND4edVSzPjG5fJXlKJ81IjXJJVLDp9CPDxbxmcAxlMFxF-Jbhmew.webp",
-  "Oscar Piastri":
-    "https://i.namu.wiki/i/Ifns07jGT5Z7ACSfDmR6pkdkhB9XvizLt3nBFYDP0NheWksnplmbK7XSHvDmeW4Gzf7YbcsfXyIkhVz5A9xzEq--AUGi6wGLvKND4edVSzPjG5fJXlKJ81IjXJJVLDp9CPDxbxmcAxlMFxF-Jbhmew.webp",
-  "Lance STROLL":
-    "https://i.namu.wiki/i/wO141ZpPvXyzyx78NXuLkpgJYhaF2yC7QSfNts0sUz5k8U5_8AC9cgkod6u9_o7AIWHiGaPmL_MjX64rNI-L_RvmWlLhou6FfRKWbwIfmjv1j6gGJ8_OT_Zd3srd1HxIOGt0psVmXb3eMq8BDKGIIw.webp",
-  "Lance Stroll":
-    "https://i.namu.wiki/i/wO141ZpPvXyzyx78NXuLkpgJYhaF2yC7QSfNts0sUz5k8U5_8AC9cgkod6u9_o7AIWHiGaPmL_MjX64rNI-L_RvmWlLhou6FfRKWbwIfmjv1j6gGJ8_OT_Zd3srd1HxIOGt0psVmXb3eMq8BDKGIIw.webp",
-  "Nico HULKENBERG":
-    "https://i.namu.wiki/i/peePRdYK9fFEg4-pYt33ydHglYxCZ10YfJWgfQBqwgwXZUai9ObeSWc0cgFedv3MFwjWTTJPmeoHjfYfrhylQnVGFSbWFwfM1gFe6iq9iL1PcTRx0Unq-ML-r2AA7POeEGfHGMWRTsO7O3IBfjd4CA.webp",
-  "Nico Hulkenberg":
-    "https://i.namu.wiki/i/peePRdYK9fFEg4-pYt33ydHglYxCZ10YfJWgfQBqwgwXZUai9ObeSWc0cgFedv3MFwjWTTJPmeoHjfYfrhylQnVGFSbWFwfM1gFe6iq9iL1PcTRx0Unq-ML-r2AA7POeEGfHGMWRTsO7O3IBfjd4CA.webp",
-  "Franco COLAPINTO":
-    "https://i.namu.wiki/i/ZW9m6seWp5Hb-vO0uRkfU-5LZZYPRgkkiA_Tdu2UTcXllLFuqj0OrLLMJJC_P8MKnv9mnmMJ0vctbBfiTtAqeqbfFciplzFl_Q3k7Hs-iuoTHC5otq770yx9UMyw2YX0PcFtjlo9Gi6cE3ztakcBDA.webp",
-  "Franco Colapinto":
-    "https://i.namu.wiki/i/ZW9m6seWp5Hb-vO0uRkfU-5LZZYPRgkkiA_Tdu2UTcXllLFuqj0OrLLMJJC_P8MKnv9mnmMJ0vctbBfiTtAqeqbfFciplzFl_Q3k7Hs-iuoTHC5otq770yx9UMyw2YX0PcFtjlo9Gi6cE3ztakcBDA.webp",
-  "Isack HADJAR":
-    "https://i.namu.wiki/i/_Rw7Q17ZUvnxCMYIwbmxNIlXdSbTkNT9FKN4Qrn2u1uTtR1hTG-B2bJPsBthdVOwiRwrrl3lC8cZZEb7Z4fZaqyeKjQoOZSMMnn0cECfqjbD9ZCCS6xnf5w3NmgP6V-DYPrGskltkVEyCu_onT_MZg.webp",
-  "Isack Hadjar":
-    "https://i.namu.wiki/i/_Rw7Q17ZUvnxCMYIwbmxNIlXdSbTkNT9FKN4Qrn2u1uTtR1hTG-B2bJPsBthdVOwiRwrrl3lC8cZZEb7Z4fZaqyeKjQoOZSMMnn0cECfqjbD9ZCCS6xnf5w3NmgP6V-DYPrGskltkVEyCu_onT_MZg.webp",
-  "Alexander ALBON":
-    "https://i.namu.wiki/i/3wXuHiPiIIgJEBXiiuPYOUGCQYG962rvefLu3TzASbUGAFg5jtcy7NEUpIMd3lyzEkHMzLtaPj3sLL6hkBzXxK6Xz8_2rq5TsTzwuhOK3f_2OQg4Q6D2e1wTCosRk06JZ09hIvnysXQYtTJU_ZnQbA.webp",
-  "Alexander Albon":
-    "https://i.namu.wiki/i/3wXuHiPiIIgJEBXiiuPYOUGCQYG962rvefLu3TzASbUGAFg5jtcy7NEUpIMd3lyzEkHMzLtaPj3sLL6hkBzXxK6Xz8_2rq5TsTzwuhOK3f_2OQg4Q6D2e1wTCosRk06JZ09hIvnysXQYtTJU_ZnQbA.webp",
-  "Gabriel BORTOLETO":
-    "https://i.namu.wiki/i/dE7xFB-IgLm6qdlksKgu19ZUg9errhbI4ZVF3xPe1kNWzTb58_h49hyIBf2MLnkCspDCvoQwdP6Fo0t5cBlXbaNV4H3RLz_6E6BHt-ZnkRdcfUEjo08Rrq1tzaWeugZ9p_8krrH5OD3nm2PnYD2kLw.webp",
-  "Gabriel Bortoleto":
-    "https://i.namu.wiki/i/dE7xFB-IgLm6qdlksKgu19ZUg9errhbI4ZVF3xPe1kNWzTb58_h49hyIBf2MLnkCspDCvoQwdP6Fo0t5cBlXbaNV4H3RLz_6E6BHt-ZnkRdcfUEjo08Rrq1tzaWeugZ9p_8krrH5OD3nm2PnYD2kLw.webp",
-  "Oliver BEARMAN":
-    "https://i.namu.wiki/i/PxnLVE3B2MSOkQdhxfTMjHJnbot_1gaHMdyjKq9oqbXtvgIE1yFcCba5BOeOqqHjyyTdCrweDnPD55YHebV1TMfMsn0B395FDpdd7DZazy71W__prkdadpXxF6uHAuVvSvEoiHQv6-1Q6teq8-3bbA.webp",
-  "Oliver Bearman":
-    "https://i.namu.wiki/i/PxnLVE3B2MSOkQdhxfTMjHJnbot_1gaHMdyjKq9oqbXtvgIE1yFcCba5BOeOqqHjyyTdCrweDnPD55YHebV1TMfMsn0B395FDpdd7DZazy71W__prkdadpXxF6uHAuVvSvEoiHQv6-1Q6teq8-3bbA.webp",
-  "Kimi ANTONELLI":
-    "https://i.namu.wiki/i/T_4IlKDpggqymyuLEXfB0ibbdPpGSuAaSwKzCgFdLG3w2zN843-sQGrwvHmxtF3csWGjEvgV78yzo_C6k1PdtdUmcWH4mzOxDqTMQAiwAiH6mqlzS1vtCvDIFpvCuhF4WfffbOdMq6dZjbH9UHERKQ.webp",
-  "Kimi Antonelli":
-    "https://i.namu.wiki/i/T_4IlKDpggqymyuLEXfB0ibbdPpGSuAaSwKzCgFdLG3w2zN843-sQGrwvHmxtF3csWGjEvgV78yzo_C6k1PdtdUmcWH4mzOxDqTMQAiwAiH6mqlzS1vtCvDIFpvCuhF4WfffbOdMq6dZjbH9UHERKQ.webp",
-  "Liam LAWSON":
-    "https://i.namu.wiki/i/MAH9P4scITqdWlSrXr2ebqaymkoZHFGBUQYlHMoBV7xh7UqzIUGj0b-4uZguo3WCmF1ubTRQT3UPR7nOceZq-jS21Li-5IMtsUH5PPvpVdV4LsGRijMQUGlLW4Xh8al7WSqXz2_LqoSFYPI0C7B1Ow.webp",
-  "Liam Lawson":
-    "https://i.namu.wiki/i/MAH9P4scITqdWlSrXr2ebqaymkoZHFGBUQYlHMoBV7xh7UqzIUGj0b-4uZguo3WCmF1ubTRQT3UPR7nOceZq-jS21Li-5IMtsUH5PPvpVdV4LsGRijMQUGlLW4Xh8al7WSqXz2_LqoSFYPI0C7B1Ow.webp",
+  "Max VERSTAPPEN": Verstappen.src || "",
+  "Max Verstappen": Verstappen.src || "",
+  "Charles LECLERC": Leclerc.src || "",
+  "Charles Leclerc": Leclerc.src || "",
+  "Lewis HAMILTON": Hamilton.src || "",
+  "Lewis Hamilton": Hamilton.src || "",
+  "George RUSSELL": Russell.src || "",
+  "George Russell": Russell.src || "",
+  "Carlos SAINZ": Sainz.src || "",
+  "Carlos Sainz": Sainz.src || "",
+  "Carlos Sainz Jr.": Sainz.src || "",
+  "Lando NORRIS": Norris.src || "",
+  "Lando Norris": Norris.src || "",
+
+  "Fernando ALONSO": Alonso.src || "",
+  "Fernando Alonso": Alonso.src || "",
+  "Esteban OCON": Ocon.src || "",
+  "Esteban Ocon": Ocon.src || "",
+  "Pierre GASLY": Gasly.src || "",
+  "Pierre Gasly": Gasly.src || "",
+  "Yuki TSUNODA": Tsunoda.src || "",
+  "Yuki Tsunoda": Tsunoda.src || "",
+  "Oscar PIASTRI": Piastri.src || "",
+  "Oscar Piastri": Piastri.src || "",
+  "Lance STROLL": Stroll.src || "",
+  "Lance Stroll": Stroll.src || "",
+  "Nico HULKENBERG": Niko.src || "",
+  "Nico Hulkenberg": Niko.src || "",
+  "Franco COLAPINTO": Franco.src || "",
+  "Franco Colapinto": Franco.src || "",
+  "Isack HADJAR": Isack.src || "",
+  "Isack Hadjar": Isack.src || "",
+  "Alexander ALBON": Albon.src || "",
+  "Alexander Albon": Albon.src || "",
+  "Gabriel BORTOLETO": Gabriel.src || "",
+  "Gabriel Bortoleto": Gabriel.src || "",
+  "Oliver BEARMAN": Oliver.src || "",
+  "Oliver Bearman": Oliver.src || "",
+  "Kimi ANTONELLI": Kimi.src || "",
+  "Kimi Antonelli": Kimi.src || "",
+  "Liam LAWSON": Liam.src || "",
+  "Liam Lawson": Liam.src || "",
 };
 
 // 드라이버 이름 한글 번역 맵
